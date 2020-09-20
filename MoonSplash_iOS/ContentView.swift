6115ffba79a75a025433d5cb5430b6d1f1d8bec2
HyejeanMOON <>
2020-09-20 13:56:02 +0900

    Initial commit

---
+++ MoonSplash_iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MoonSplash_iOS
+//
+//  Created by Haichen Wen on 2020/09/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MoonSplash_iOS/MoonSplash_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonSplash_iOSApp.swift
+//  MoonSplash_iOS
+//
+//  Created by Haichen Wen on 2020/09/20.
+//
+
+import SwiftUI
+
+@main
+struct MoonSplash_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
